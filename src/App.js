2bbef76e4f69811790c86063ccd414d61e65e09d
kimnoca <kimnoca@naver.com>
2023-09-27 19:16:56 +0900

    Feat: Merge SignUp and Login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import "./App.css";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Login from "./Login";
 import SignUp from "./SignUp";
 
 function App() {
     return (
-        <div className="App">
-            <Login />
-            <SignUp />
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" Component={Login} />
+                <Route path="/Signup" Component={SignUp} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
