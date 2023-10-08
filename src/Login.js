a106bc8d8b2e6dc7af82a7df25cd0b87f757cde6
hjoo830 <hjoo830@naver.com>
2023-10-08 17:09:19 +0900

    Feat: test

--- src/Login.js
+++ src/Login.js
@@ -2,12 +2,33 @@ import "./Login.css";
 import kakaoLogo from "./kakaoLogo.png";
 
 function Login() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("https://", {
+      method: "POST",
+      body: JSON.stringify({
+        email: this.state.idVal,
+        password: this.state.pwVal,
+      }),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        if (response.token) {
+          //토큰 값 존재하면
+          localStorage.setItem("token", response.token); //토큰값 브라우저의 로컬스토리지에 저장
+          this.props.history.push("/main"); //메인페이지로 이동
+        } else {
+          alert("로그인 실패");
+        }
+      });
+  };
+
   return (
     <div className="Login">
       <h2 className="welcome">
         <span className="modira">MODIRA</span>에 오신 것을 환영합니다!
       </h2>
-      <form method="POST">
+      <form onSubmit={handleSubmit}>
         <input id="email" name="email" placeholder="E-MAIL"></input>
         <input
           id="password"
