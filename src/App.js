f13280cdce3fc052bcd0472600e2eeec3635d34d
kimnoca <kimnoca@naver.com>
2023-11-04 00:49:32 +0900

    Feat : refresh token 수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
-import { actionCreators } from "./redux/user";
+// import { actionCreators } from "./redux/user";
 import React, { useEffect } from "react";
-import { getCookie } from "./config/cookie";
+// import { getCookie } from "./config/cookie";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./page/Login";
@@ -9,29 +9,29 @@ import SignUp from "./page/SignUp";
 import Mypage from "./page/Mypage";
 import Home from "./page/Home";
 import AddGroup from "./page/Group/AddGroup";
-import { useDispatch } from "react-redux";
+// import { useDispatch } from "react-redux";
 
-function App() { 
-  const dispatch = useDispatch();
-  
-  useEffect(() => {
-  //쿠키에 저장된 액세스 토큰이 존재할 때만 서버에 검증 요청
-   if(getCookie("is_login")){
-     dispatch(actionCreators.loginCheckDB());
-   }
-  }, []);
+function App() {
+    // const dispatch = useDispatch();
 
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" Component={Login} />
-        <Route path="/Signup" Component={SignUp} />
-        <Route path="/Mypage" Component={Mypage} />
-        <Route path="/Home" Component={Home} />
-        <Route path="/AddGroup" Component={AddGroup}/>
-      </Routes>
-    </BrowserRouter>
-  );
+    // useEffect(() => {
+    // //쿠키에 저장된 액세스 토큰이 존재할 때만 서버에 검증 요청
+    //  if(getCookie("is_login")){
+    //    dispatch(actionCreators.loginCheckDB());
+    //  }
+    // }, []);
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" Component={Login} />
+                <Route path="/Signup" Component={SignUp} />
+                <Route path="/Mypage" Component={Mypage} />
+                <Route path="/Home" Component={Home} />
+                <Route path="/AddGroup" Component={AddGroup} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/redux/user.js
+++ src/redux/user.js
@@ -1,10 +1,8 @@
 //Redux 모듈 내 API 통신
 import { createAction, handleActions } from "redux-actions";
-import {produce} from "immer";
-import {setCookie, removeCookie} from "../config/cookie";
+import { produce } from "immer";
+import { setCookie, removeCookie } from "../config/cookie";
 import axios from "axios";
-
-
 
 //Action
 const SET_USER = "SET_USER";
@@ -13,79 +11,82 @@ const LOG_OUT = "LOG_OUT";
 
 //initialState
 const initialState = {
-    user:  null,
-    is_login : false, 
+    user: null,
+    is_login: false,
 };
 
-
 //Action Creator
-const logOut = createAction(LOG_OUT, (user) => ({user}));
-const setUser = createAction(SET_USER, (user) => ({user}));
-const getUser = createAction(GET_USER, (user)=> ({user}));
+const logOut = createAction(LOG_OUT, user => ({ user }));
+const setUser = createAction(SET_USER, user => ({ user }));
+const getUser = createAction(GET_USER, user => ({ user }));
 
 //middleware actions
 
 //Login
-const loginDB = (body) => {
+const loginDB = body => {
     return function (dispatch) {
-        axios.post("http://localhost:8000/user/login/", body, {
-            headers: {
-                "Content-Type": "application/json",
-            },
-        })
-        .then((res) => {
-            dispatch(
-                setUser({
-                    email : res.data.email,
-                })
-            )
-            const is_login = res.data.token.access;
-            localStorage.setitem('refresh-token', res.data.token.refresh);
-            setCookie("is_login", `${is_login}`);
-        })
-        .catch((error) => {
-            console.log(error);
-        })
-    }
-}
+        axios
+            .post("http://localhost:8000/user/login/", body, {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            })
+            .then(res => {
+                dispatch(
+                    setUser({
+                        email: res.data.email,
+                    })
+                );
+                const is_login = res.data.token.access;
+                console.log("refresh-token : ", res.data.token.refresh);
+                localStorage.setItem("refresh-token", res.data.token.refresh);
+                setCookie("is_login", `${is_login}`);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    };
+};
 
 // SignUp
-const signupDB = (body) => {
-    return function(){
-        axios.post("http://localhost:8000/users/register/", body)
-        .then((res) => {
-            console.log(res);
-        })
-        .catch((error) => {
-            console.log(error);
-        })
-    }   
-}
+const signupDB = body => {
+    return function () {
+        axios
+            .post("http://localhost:8000/users/register/", body)
+            .then(res => {
+                console.log(res);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    };
+};
 
 // 로그인 유지
 const loginCheckDB = () => {
-    return function() {
+    return function () {
         const token = localStorage.getItem("refresh-token");
         console.log(token);
-        axios.post("127.0.0.1:8000/user/refresh/",token, {
-            headers : {
-                "Content-Type": "application/json",
-                Authorization : `Bearer${token}`,
-            },
-        })
-        .then((res) => {
-            const is_login = res.data.token.access;
-            setCookie("is_login", `${is_login}`);
-        })
-        .catch((error) => {
-            console.log(error);
-        })
-    }
-}
+        axios
+            .post("127.0.0.1:8000/user/refresh/", token, {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer${token}`,
+                },
+            })
+            .then(res => {
+                const is_login = res.data.token.access;
+                setCookie("is_login", `${is_login}`);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    };
+};
 
 // logout
 const logoutDB = () => {
-    return function(dispatch, {history}){
+    return function (dispatch, { history }) {
         dispatch(logOut());
         history.replace("/Home");
     };
@@ -94,20 +95,20 @@ const logoutDB = () => {
 //불변성 유지
 export default handleActions(
     {
-        [SET_USER] : (state,action) => 
-            produce(state, (draft) => {
+        [SET_USER]: (state, action) =>
+            produce(state, draft => {
                 draft.user = action.payload.user;
                 draft.accessToken = true;
             }),
-        
-        [LOG_OUT] : (state,action) => 
-            produce(state,(draft) => {
+
+        [LOG_OUT]: (state, action) =>
+            produce(state, draft => {
                 localStorage.removeItem("refresh-token");
                 removeCookie("is_login");
                 draft.user = null;
                 draft.accessToken = false;
             }),
-        [GET_USER] : (state,action) => produce(state, (draft) => {}),
+        [GET_USER]: (state, action) => produce(state, draft => {}),
     },
     initialState
 );
