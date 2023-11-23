6e7da51a24f4688867410f5f515d0899df883d61
hjoo830 <hjoo830@naver.com>
2023-11-23 21:57:03 +0900

    Feat: Q&A 등록 post 요청

--- src/page/QnA/AddQnA.js
+++ src/page/QnA/AddQnA.js
@@ -1,6 +1,31 @@
+import React, { useState } from "react";
+import axios from "axios";
+
 import "../../css/AddQnA.css";
 
 function AddQnA() {
+  const [qnaTitle, setQnaTitle] = useState("");
+  const [questionContent, setQuestionContent] = useState("");
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const url = "http://127.0.0.1:8000/qna/posts";
+
+      const postData = {
+        title: qnaTitle,
+        content: questionContent,
+      };
+
+      const response = await axios.post(url, postData);
+
+      console.log("POST 요청 성공:", response.data);
+    } catch (error) {
+      console.error("POST 요청 실패:", error);
+    }
+  };
+
   return (
     <div>
       <div className="addQnA">
@@ -9,17 +34,26 @@ function AddQnA() {
           <br />
           궁금증을 해결해보세요!
         </p>
-        <form className="addQnAForm">
+        <form className="addQnAForm" onSubmit={handleFormSubmit}>
           <input
             id="qnaTitle"
             name="qnaTitle"
             placeholder="제목을 작성해주세요"
             required
+            value={qnaTitle}
+            onChange={(e) => setQnaTitle(e.target.value)}
           ></input>
-          <textarea className="qusetionContent" required></textarea>
+          <textarea
+            className="qusetionContent"
+            required
+            value={questionContent}
+            onChange={(e) => setQuestionContent(e.target.value)}
+          ></textarea>
           <div className="registerQnAButtons">
             <button className="cancelButton">취소하기</button>
-            <button className="registerQnAButton">등록하기</button>
+            <button type="submit" className="registerQnAButton">
+              등록하기
+            </button>
           </div>
         </form>
       </div>
