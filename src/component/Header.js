08785a8076bc87e7de7d8572e203b6069e49b95f
hjoo830 <hjoo830@naver.com>
2023-11-23 20:24:38 +0900

    Feat: header status

--- src/component/Header.js
+++ src/component/Header.js
@@ -4,7 +4,7 @@ import dropdownImg from "../asset/dropdownImg.png";
 import "../css/Header.css";
 
 function Header() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
 
   return (
     <header className="header">
@@ -13,16 +13,15 @@ function Header() {
         {isLoggedIn ? (
           // 로그인된 상태일 때
           <>
-            <a href="/newpost" className="link">
+            <a href="/" className="link">
               새글쓰기
             </a>
             <img
-              src={dropdownImg} // 이미지의 경로를 넣으세요
+              src={dropdownImg}
               alt="Dropdown"
               className="dropdownImg"
               onClick={() => {
-                // 이미지를 클릭하면 드롭다운 메뉴가 열리도록 로직을 추가하세요
-                // 예: 드롭다운 상태를 관리하는 state를 추가하고 클릭 시 상태를 변경하여 드롭다운을 열거나 닫습니다.
+                // 드롭다운 추가
               }}
             />
           </>
