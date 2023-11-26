b7a4771192c6387d169662c206f1a970f213d3b9
kimnoca <kimnoca@naver.com>
2023-11-26 21:53:51 +0900

    feat : Q&A 상세 페이지 이동

--- src/component/GroupContainer.js
+++ src/component/GroupContainer.js
@@ -3,61 +3,68 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 import axios from "axios";
 import "../css/GroupContainer.css";
-import { useNavigate } from "react-router";
+import defaultImg from "../asset/defaultImg.png";
+import { Link } from "react-router-dom";
 
-function GroupContainer({url}) {
-    const navigate = useNavigate();
-    console.log("url" , url);
+function GroupContainer({ url }) {
+    console.log("url", url);
 
     const [groupList, setGroupList] = useState([]);
-    const [groupID, setGroupID] = useState(); //상세페이지 id 설정
-    const [profileURL , setprofileURL] = useState(); //이미지 url 설정
 
     useEffect(() => {
         axios({
-            method : "GET",
-            url : url,
-            headers : {
+            method: "GET",
+            url: url,
+            headers: {
                 "Content-Type": "application/json",
             },
-        }).then((res) => {
-            console.log(res.data);
-            setGroupList(res.data)
-            setGroupID(res.data.id);
-            setprofileURL("localhost:8000"+res.data.author_profile_image);
         })
-        .then((err) => {
-            console.log('error : ',err);
-        })
-    },[url]);
+            .then(res => {
+                console.log(res.data);
+                setGroupList(res.data);
+            })
+            .then(err => {
+                console.log("error : ", err);
+            });
+    }, [url]);
 
-    // 컨테이너 클릭 시 해당 컨테이너의 상세페이지로 이동
-    const clickGroup= () => {
-        navigate(`/gathering/posts/${groupID}`);
-    }
-
-    return(
+    return (
         <div className="Main_Group">
             {/* get은 group.map((item)) */}
-            {groupList.map((group) => (
-                <div key={group.id} className="groupContainer" onClick={clickGroup}>
-                    <div className="title_INFO">
-                        <div className="titleINFO">
-                            <div className="title"> {group.title} </div>
-                            <div className="deadline"> {group.deadline} </div>
+            {groupList.map(group => (
+                <Link
+                    to={`/GroupPage/${group.id}`}
+                    style={{ textDecoration: "none", color: "#000000" }}
+                    className="Group_Link"
+                >
+                    <div key={group.id} className="groupContainer">
+                        <div className="title_INFO">
+                            <div className="titleINFO">
+                                <div className="title"> {group.title} </div>
+                                <div className="deadline">
+                                    {" "}
+                                    {group.deadline}{" "}
+                                </div>
+                            </div>
+                            <div className="type"> {group.tag} </div>
                         </div>
-                    <div className="type"> {group.tag} </div>
-                </div>
-                <div className="summary"> {group.summary} </div>
-                <div className="profileINFO">
-                    <img src={profileURL} alt="profile" className="profile_IMG" />
-                    <div> {group.author_nickname} </div>
-                </div>
-                </div>
+                        <div className="summary"> {group.summary} </div>
+                        <div className="profileINFO">
+                            <img
+                                src={
+                                    "http://localhost:8000" +
+                                        group.author_profile_image || defaultImg
+                                }
+                                alt="profile"
+                                className="profile_IMG"
+                            />
+                            <div> {group.author_nickname} </div>
+                        </div>
+                    </div>
+                </Link>
             ))}
         </div>
-    )
-
+    );
 }
 
 export default GroupContainer;
--- src/component/QnAContainer.js
+++ src/component/QnAContainer.js
@@ -1,60 +1,70 @@
 import React, { useEffect } from "react";
 import "../css/QnAContainer.css";
-import { useNavigate } from "react-router";
+import { Link } from "react-router-dom";
 import axios from "axios";
 import { useState } from "react";
+import defaultImg from "../asset/defaultImg.png";
 
-
-function QnAContainer({url}) {
-    const navigate = useNavigate();
-    const [QnAList , setQnAList] = useState([]);
-    const [QnAID, setQnAID] = useState(); //상세페이지 id 설정
-    const [profileURL , setprofileURL] = useState(); //이미지 url 설정
+function QnAContainer({ url }) {
+    const [QnAList, setQnAList] = useState([]);
 
     useEffect(() => {
         axios({
-            method : "GET",
-            url : url,
-            headers : {
-                "Content-Type" : "application/json",
+            method: "GET",
+            url: url,
+            headers: {
+                "Content-Type": "application/json",
             },
-        }).then((res) => {
-            console.log(res.data);
-            setQnAList(res.data);
-            setQnAID(res.data.id);
-            setprofileURL("localhost:8000"+res.data.author_profile_image);        })
-        .then((err) => {
-            console.log("error : ", err);
         })
-    },[url]);
+            .then(res => {
+                console.log(res.data);
+                setQnAList(res.data);
+            })
+            .then(err => {
+                console.log("error : ", err);
+            });
+    }, [url]);
 
-     // 컨테이너 클릭 시 해당 컨테이너의 상세페이지로 이동
-     const clickQnA= () => {
-        navigate(`/QnAPage/${QnAID}`);
-    }
-
-    return(
+    return (
         <div className="QnA_Main" style={{ marginBottom: "40px" }}>
-            {QnAList.map((qna) => (
-                <div key={qna.id} className="Main_QnA" onClick={clickQnA}>
-                    <div className="QnA_titleBox">
-                        {qna.status ? (
-                            <div className="stateFlase"> 미해결 </div>
-                        ) : (
-                            <div className="stateTrue"> 해결 </div>
-                        )}
-                        <div className="title"> {qna.title} </div>
+            {QnAList.map(qna => (
+                <Link
+                    to={`/QnAPage/${qna.id}`}
+                    style={{ textDecoration: "none", color: "#000000" }}
+                >
+                    <div key={qna.id} className="Main_QnA">
+                        <div className="QnA_titleBox">
+                            {qna.status ? (
+                                <div className="stateFalse"> 미해결 </div>
+                            ) : (
+                                <div className="stateTrue"> 해결 </div>
+                            )}
+                            <div className="title"> {qna.title} </div>
+                        </div>
+                        <div className="summary"> {qna.summary} </div>
+                        <div className="QnA_content">
+                            <img
+                                src={
+                                    "http://localhost:8000" +
+                                        qna.author_profile_image || defaultImg
+                                }
+                                alt="기본 이미지"
+                                className="profile"
+                            />
+                            <div className="nickname">
+                                {" "}
+                                {qna.author_nickname}{" "}
+                            </div>
+                            <div className="created_at">
+                                {" "}
+                                등록일 : {qna.created_at}{" "}
+                            </div>
+                        </div>
                     </div>
-                    <div className="summary"> {qna.summary} </div>
-                    <div className="QnA_content">
-                        <img src={profileURL} alt="기본 이미지" className="profile" />
-                        <div className="nickname"> {qna.author_nickname} </div>
-                        <div className="created_at"> 등록일 : {qna.created_at} </div>
-                    </div>
-                </div>
+                </Link>
             ))}
-    </div>
-    )
+        </div>
+    );
 }
 
-export default  QnAContainer;
+export default QnAContainer;
