0c5b665d253ec6a3872d82da4cc606d5e6b11fd6
kimnoca <kimnoca@naver.com>
2023-11-27 14:34:31 +0900

    Fix : 소모임 댓글 수정

--- src/component/GroupComment.js
+++ src/component/GroupComment.js
@@ -2,75 +2,75 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function GroupComment({ id }) {
-  const [commentData, setCommentData] = useState({
-    profileImg: "",
-    commentContent: "",
-    nickname: "",
-  });
-  const token = localStorage.getItem("access-token");
+    const [commentData, setCommentData] = useState({
+        profileImg: "",
+        commentContent: "",
+        nickname: "",
+    });
 
-  const containerStyle = {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    width: "100%", // .commentLayout의 width 스타일
-    gap: "10px", // .commentLayout의 gap 스타일
-  };
+    // const [commentData, setCommentData] = useState([]); //댓글 정보
 
-  const commentInputStyle = {
-    height: "50px",
-    borderColor: "#D9D9D9",
-    borderRadius: "10px",
-    resize: "none",
-    fontSize: "20px",
-    flexGrow: 1, // flex-grow 속성
-  };
-
-  const commentContentStyle = {
-    marginLeft: "40px",
-    marginBottom: "50px",
-  };
-
-  useEffect(() => {
-    const options = {
-      url: `http://127.0.0.1:8000/gathering/posts/${id}/comments/`,
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json;charset=UTF-8",
-        Authorization: `Bearer ${token}`,
-      },
+    const containerStyle = {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%", // .commentLayout의 width 스타일
+        gap: "10px", // .commentLayout의 gap 스타일
     };
 
-    axios(options)
-      .then((response) => {
-        // 서버로부터 받은 데이터로 상태를 업데이트
-        setCommentData({
-          profileImg: response.data.profileImg,
-          commentContent: response.data.commentContent,
-          nickname: response.data.nickname,
-        });
-      })
-      .catch((error) => console.error(error));
-  }, [token, id]);
-  const { profileImg, commentContent, nickname } = commentData;
+    const commentInputStyle = {
+        height: "50px",
+        borderColor: "#D9D9D9",
+        borderRadius: "10px",
+        resize: "none",
+        fontSize: "20px",
+        flexGrow: 1, // flex-grow 속성
+    };
 
-  return (
-    <div className="groupComment" style={containerStyle}>
-      <div className="userInfo">
-        <img
-          src={profileImg}
-          className="profileImg"
-          alt="profileImg"
-          style={commentInputStyle}
-        ></img>
-        <p>{nickname}</p>
-      </div>
-      <p className="commentContent" style={commentContentStyle}>
-        {commentContent}
-      </p>
-    </div>
-  );
+    const commentContentStyle = {
+        marginLeft: "40px",
+        marginBottom: "50px",
+    };
+
+    useEffect(() => {
+        console.log("아이디: ", id);
+        axios({
+            method: "GET",
+            url: `http://127.0.0.1:8000/gathering/posts/${id}/comments/`,
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then(response => {
+                // 서버로부터 받은 데이터로 상태를 업데이트
+                console.log("데이터 : ", response.data);
+                setCommentData({
+                    profileImg: response.data[0].author_profile_image,
+                    commentContent: response.data[0].content,
+                    nickname: response.data[0].writer,
+                });
+                console.log("댓글 내용: ", commentData);
+            })
+            .catch(error => console.error(error));
+    }, [id]);
+    // const { profileImg, commentContent, nickname } = commentData;
+
+    return (
+        <div className="groupComment" style={containerStyle}>
+            <div className="userInfo">
+                <img
+                    src={"http://localhost:8000" + commentData.profileImg}
+                    className="profileImg"
+                    alt="profileImg"
+                    style={commentInputStyle}
+                ></img>
+                <p>{commentData.nickname}</p>
+            </div>
+            <p className="commentContent" style={commentContentStyle}>
+                {commentData.commentContent}
+            </p>
+        </div>
+    );
 }
 
 export default GroupComment;
