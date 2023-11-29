f048c85cd512a3f6f81764d0585892fc222781a4
hjoo830 <hjoo830@naver.com>
2023-11-29 20:35:38 +0900

    Feat: 취소하기 navigate(-1)

--- src/page/QnA/AddQnA.js
+++ src/page/QnA/AddQnA.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
-
 import "../../css/AddQnA.css";
+import { useNavigate } from "react-router-dom";
 
 function AddQnA() {
   const [qnaTitle, setQnaTitle] = useState("");
   const [qnaSummary, setQnaSummary] = useState("");
   const [questionContent, setQuestionContent] = useState("");
   const token = localStorage.getItem("access-token");
+  const navigate = useNavigate();
 
   const handleFormSubmit = async (e) => {
     e.preventDefault();
@@ -22,7 +23,7 @@ function AddQnA() {
       },
       data: {
         title: qnaTitle,
-        summary : qnaSummary,
+        summary: qnaSummary,
         content: questionContent,
       },
     };
@@ -30,6 +31,10 @@ function AddQnA() {
     axios(options)
       .then((response) => console.log(response))
       .catch((error) => console.error(error));
+  };
+
+  const handleCancel = () => {
+    navigate(-1);
   };
 
   return (
@@ -64,7 +69,9 @@ function AddQnA() {
             onChange={(e) => setQuestionContent(e.target.value)}
           ></textarea>
           <div className="registerQnAButtons">
-            <button className="cancelButton">취소하기</button>
+            <button className="cancelButton" onClick={handleCancel}>
+              취소하기
+            </button>
             <button type="submit" className="registerQnAButton">
               등록하기
             </button>
