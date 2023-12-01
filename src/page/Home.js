965eb059814581392feb548868e363f0e264aa0d
kimnoca <kimnoca@naver.com>
2023-12-01 17:29:52 +0900

    fix : 모집중/모집완료 수정

--- src/page/Home.js
+++ src/page/Home.js
@@ -7,227 +7,240 @@ import QnAContainer from "../component/QnAContainer";
 import "../css/Home.css";
 
 function Home() {
-  //소모임, Q&A 화면 렌더링 -> 소모임이면 true, Q&A이면 false
-  const [isGroup, setIsGroup] = useState(true);
+    //소모임, Q&A 화면 렌더링 -> 소모임이면 true, Q&A이면 false
+    const [isGroup, setIsGroup] = useState(true);
 
-  //전송 데이터 폼
-  const method = "GET"; //method
-  const [url, setUrl] = useState("http://localhost:8000/gathering/posts/"); //url 설정
-  // axios 요청 header
-  const headers = {
-    "Content-Type": "application/json",
-  };
+    //전송 데이터 폼
+    const method = "GET"; //method
+    const [url, setUrl] = useState("http://localhost:8000/gathering/posts/"); //url 설정
+    // axios 요청 header
+    const headers = {
+        "Content-Type": "application/json",
+    };
 
-  //소모임 컨테이너
-  const clickGroup = () => {
-    setIsGroup(true);
-    setUrl("http://localhost:8000/gathering/posts/");
-  };
+    //소모임 컨테이너
+    const clickGroup = () => {
+        setIsGroup(true);
+        setUrl("http://localhost:8000/gathering/posts/");
+    };
 
-  // Q&A 컨테이너
-  const clickQnA = () => {
-    setIsGroup(false);
-    setUrl("http://localhost:8000/qna/posts/");
-  };
+    // Q&A 컨테이너
+    const clickQnA = () => {
+        setIsGroup(false);
+        setUrl("http://localhost:8000/qna/posts/");
+    };
 
-  //검색
-  const updateSearchURL = (newURL) => {
-    setUrl(newURL);
-  };
+    //검색
+    const updateSearchURL = newURL => {
+        setUrl(newURL);
+    };
 
-  return (
-    <div className="home_page">
-      <Banner />
-      <div className="nav">
-        <button
-          className="group"
-          onClick={clickGroup}
-          style={{ cursor: "pointer" }}>
-          {" "}
-          소모임{" "}
-        </button>
-        <button
-          href="/"
-          className="qna"
-          onClick={clickQnA}
-          style={{ cursor: "pointer" }}>
-          Q&A{" "}
-        </button>
-      </div>
-      <div className="groupList" style={{ marginTop: "30px" }}>
-        {isGroup ? (
-          <div className="home-Container">
-            <Search isGroup={isGroup} updateSearchURL={updateSearchURL} />
-            <div style={{ marginLeft: "3%", marginBottom: "100px" }}>
-              <GroupContainer
-                method={method}
-                url={url}
-                headers={headers}
-                modify={false}
-              />
+    return (
+        <div className="home_page">
+            <Banner />
+            <div className="nav">
+                <button
+                    className="group"
+                    onClick={clickGroup}
+                    style={{ cursor: "pointer" }}
+                >
+                    {" "}
+                    소모임{" "}
+                </button>
+                <button
+                    href="/"
+                    className="qna"
+                    onClick={clickQnA}
+                    style={{ cursor: "pointer" }}
+                >
+                    Q&A{" "}
+                </button>
             </div>
-          </div>
-        ) : (
-          <div className="home-Container">
-            <Search isGroup={isGroup} updateSearchURL={updateSearchURL} />
-            <div style={{ marginLeft: "10%" }}>
-              <QnAContainer
-                method={method}
-                url={url}
-                headers={headers}
-                modify={false}
-              />
+            <div className="groupList" style={{ marginTop: "30px" }}>
+                {isGroup ? (
+                    <div className="home-Container">
+                        <Search
+                            isGroup={isGroup}
+                            updateSearchURL={updateSearchURL}
+                        />
+                        <div
+                            style={{ marginLeft: "3%", marginBottom: "100px" }}
+                        >
+                            <GroupContainer
+                                method={method}
+                                url={url}
+                                headers={headers}
+                                modify={false}
+                            />
+                        </div>
+                    </div>
+                ) : (
+                    <div className="home-Container">
+                        <Search
+                            isGroup={isGroup}
+                            updateSearchURL={updateSearchURL}
+                        />
+                        <div style={{ marginLeft: "10%" }}>
+                            <QnAContainer
+                                method={method}
+                                url={url}
+                                headers={headers}
+                                modify={false}
+                            />
+                        </div>
+                    </div>
+                )}
             </div>
-          </div>
-        )}
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
 
 export default Home;
 
 export function Search({ isGroup, updateSearchURL }) {
-  const [all, setAll] = useState(true); // ALL 선택
-  const [title, setTitle] = useState(""); // 키워드 검색
-  const [ing, setIng] = useState(true); // 모집중 검색
-  const [tag, setTag] = useState(""); //태그 검색
-  const [typeDropdownOpen, setTypeDropdownOpen] = useState(false); // 타입 검색 드롭다운
-  const [activeType, setActiveType] = useState(""); //타입 버튼 선택 css
+    const [all, setAll] = useState(true); // ALL 선택
+    const [title, setTitle] = useState(""); // 키워드 검색
+    const [ing, setIng] = useState(true); // 모집중 검색
+    const [tag, setTag] = useState(""); //태그 검색
+    const [typeDropdownOpen, setTypeDropdownOpen] = useState(false); // 타입 검색 드롭다운
+    const [activeType, setActiveType] = useState(""); //타입 버튼 선택 css
 
-  const [searchUrl, setSearchUrl] = useState(
-    `http://localhost:8000/gathering/posts/search?title=${title}&tag=${tag}&status=${ing}`
-  );
-
-  useEffect(() => {
-    // console.log("Updated state values:", { all, title, ing, tag });
-    // console.log("Updated searchUrl: ", searchUrl);
-  }, [all, title, ing, tag, searchUrl]);
-
-  // 폼 제출
-  const handleURLChange = (e) => {
-    e.preventDefault();
-    // console.log("실행");
-    // console.log("검색:", searchUrl);
-    updateSearchURL(searchUrl);
-    setTitle("");
-  };
-
-  //ALL 버튼 선택
-  const handleAllChange = (e) => {
-    e.preventDefault();
-    setAll(!all);
-    setTypeDropdownOpen(!typeDropdownOpen); //드롭다운
-    setSearchUrl(
-      `http://localhost:8000/gathering/posts/search?title=&tag=&status=`
+    const [searchUrl, setSearchUrl] = useState(
+        `http://localhost:8000/gathering/posts/search?title=${title}&tag=${tag}&status=${ing}`
     );
-    // console.log("ALL 버튼 URL: ", searchUrl);
-    if (!all) {
-      // console.log("ALL(true) URL: ", searchUrl);
-      handleURLChange(e);
-    }
-  };
 
-  //Type 버튼 선택
-  const handleTagChange = (e) => {
-    e.preventDefault();
-    const newTag = e.target.value;
-    // console.log("target value : ", newTag);
-    setActiveType(newTag);
-    setSearchUrl(
-      `http://localhost:8000/gathering/posts/search?title=&tag=${newTag}&status=`
-    );
-    // console.log("타겟 벨류:", tag);
-    // console.log("태그 검색 URL : ", searchUrl);
-    handleURLChange(e);
-  };
+    useEffect(() => {
+        // console.log("Updated state values:", { all, title, ing, tag });
+        // console.log("Updated searchUrl: ", searchUrl);
+    }, [all, title, ing, tag, searchUrl]);
 
-  // 키워드 검색
-  const handleTitleChange = (e) => {
-    e.preventDefault();
-    const newTitle = e.target.value;
-    setTitle(newTitle);
-    if (isGroup) {
-      setSearchUrl(
-        `http://localhost:8000/gathering/posts/search?title=${newTitle}&tag=&status=`
-      );
-    } else {
-      setSearchUrl(
-        `http://localhost:8000/qna/posts/search?title=${newTitle}&tag=&status=`
-      );
-    }
-  };
+    // 폼 제출
+    const handleURLChange = e => {
+        e.preventDefault();
+        // console.log("실행");
+        // console.log("검색:", searchUrl);
+        updateSearchURL(searchUrl);
+        setTitle("");
+    };
 
-  // 모집중 검색
-  const handleStatusChange = (e) => {
-    e.preventDefault();
-    let newStatus = !ing; // Toggle the value
-    setIng(newStatus);
-    // console.log("상태", newStatus);
-    if (ing) {
-      setSearchUrl(
-        `http://localhost:8000/gathering/posts/search?title=&tag=&status=`
-      );
-    } else {
-      setSearchUrl(
-        `http://localhost:8000/gathering/posts/search?title=&tag=&status=${newStatus}`
-      );
-    }
-    handleURLChange(e);
-  };
+    //ALL 버튼 선택
+    const handleAllChange = e => {
+        e.preventDefault();
+        setAll(!all);
+        setTypeDropdownOpen(!typeDropdownOpen); //드롭다운
+        setSearchUrl(
+            `http://localhost:8000/gathering/posts/search?title=&tag=&status=`
+        );
+        // console.log("ALL 버튼 URL: ", searchUrl);
+        if (!all) {
+            // console.log("ALL(true) URL: ", searchUrl);
+            handleURLChange(e);
+        }
+    };
 
-  const style = {};
+    //Type 버튼 선택
+    const handleTagChange = e => {
+        e.preventDefault();
+        const newTag = e.target.value;
+        // console.log("target value : ", newTag);
+        setActiveType(newTag);
+        setSearchUrl(
+            `http://localhost:8000/gathering/posts/search?title=&tag=${newTag}&status=`
+        );
+        // console.log("타겟 벨류:", tag);
+        // console.log("태그 검색 URL : ", searchUrl);
+        handleURLChange(e);
+    };
 
-  return (
-    <div>
-      {isGroup ? (
-        <div className="search-container">
-          <form className="group_search" onSubmit={handleURLChange}>
-            <div className="search-form">
-              <div className="search-group">
-                <button type="button" onClick={handleAllChange} className="all">
-                  {" "}
-                  ALL{" "}
-                </button>
-                <div className="type-dropdown">
-                  {typeDropdownOpen ? (
-                    <TypeBtn
-                      handleChange={handleTagChange}
-                      activeType={activeType}
-                    />
-                  ) : null}
+    // 키워드 검색
+    const handleTitleChange = e => {
+        e.preventDefault();
+        const newTitle = e.target.value;
+        setTitle(newTitle);
+        if (isGroup) {
+            setSearchUrl(
+                `http://localhost:8000/gathering/posts/search?title=${newTitle}&tag=&status=`
+            );
+        } else {
+            setSearchUrl(
+                `http://localhost:8000/qna/posts/search?title=${newTitle}&tag=&status=`
+            );
+        }
+    };
+
+    // 모집중 검색
+    const handleStatusChange = e => {
+        e.preventDefault();
+        let newStatus = !ing; // Toggle the value
+        setIng(newStatus);
+        console.log("상태", newStatus);
+        if (ing) {
+            setSearchUrl(
+                `http://localhost:8000/gathering/posts/search?title=&tag=&status=`
+            );
+        } else {
+            setSearchUrl(
+                `http://localhost:8000/gathering/posts/search?title=&tag=&status=${!newStatus}`
+            );
+        }
+        handleURLChange(e);
+    };
+
+    return (
+        <div>
+            {isGroup ? (
+                <div className="search-container">
+                    <form className="group_search" onSubmit={handleURLChange}>
+                        <div className="search-form">
+                            <div className="search-group">
+                                <button
+                                    type="button"
+                                    onClick={handleAllChange}
+                                    className="all"
+                                >
+                                    {" "}
+                                    ALL{" "}
+                                </button>
+                                <div className="type-dropdown">
+                                    {typeDropdownOpen ? (
+                                        <TypeBtn
+                                            handleChange={handleTagChange}
+                                            activeType={activeType}
+                                        />
+                                    ) : null}
+                                </div>
+                            </div>
+                            <button
+                                onClick={handleStatusChange}
+                                className={!ing ? "ingTrue" : "ingFalse"}
+                            >
+                                {" "}
+                                모집중
+                            </button>
+                        </div>
+                        <input
+                            id="search-keyword"
+                            type="text"
+                            className="keyword"
+                            placeholder="원하는 소모임을 검색해보세요"
+                            onChange={handleTitleChange}
+                            value={title}
+                        />
+                    </form>
                 </div>
-              </div>
-              <button
-                onClick={handleStatusChange}
-                className={!ing ? "ingTrue" : "ingFalse"}>
-                {" "}
-                모집중
-              </button>
-            </div>
-            <input
-              id="search-keyword"
-              type="text"
-              className="keyword"
-              placeholder="원하는 소모임을 검색해보세요"
-              onChange={handleTitleChange}
-              value={title}
-            />
-          </form>
+            ) : (
+                <div className="QnAList">
+                    <form className="qna_serach" onSubmit={handleURLChange}>
+                        <input
+                            id="keyword"
+                            type="text"
+                            className="keyword"
+                            placeholder="궁금한 질문을 검색해보세요"
+                            onChange={handleTitleChange}
+                        />
+                    </form>
+                </div>
+            )}
         </div>
-      ) : (
-        <div className="QnAList">
-          <form className="qna_serach" onSubmit={handleURLChange}>
-            <input
-              id="keyword"
-              type="text"
-              className="keyword"
-              placeholder="궁금한 질문을 검색해보세요"
-              onChange={handleTitleChange}
-            />
-          </form>
-        </div>
-      )}
-    </div>
-  );
+    );
 }
--- src/page/Login.js
+++ src/page/Login.js
@@ -9,86 +9,98 @@ import kakaoLogo from "../asset/kakaoLogo.png";
 import { useDispatch } from "react-redux";
 
 function Login() {
-  //react-hook-form 사용
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const { register, handleSubmit } = useForm();
+    //react-hook-form 사용
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const { register, handleSubmit } = useForm();
 
-  //Submit
-  const onSubmit = async (data) => {
-    // console.log(data); //콘솔 확인
+    //Submit
+    const onSubmit = async data => {
+        // console.log(data); //콘솔 확인
 
-    const { email, password } = data;
+        const { email, password } = data;
 
-    let body = {
-      email: email,
-      password: password,
+        let body = {
+            email: email,
+            password: password,
+        };
+
+        // console.log("body", body);
+
+        dispatch(actionCreators.loginDB(body, navigate));
     };
 
-    // console.log("body", body);
+    //kakao 로그인
+    const REST_API_KEY = "e1e4af98ca9f6131da2d779f616737bc";
+    const REDIRECT_URI = "http://127.0.0.1:3000/user/kakao/callback/";
+    const link = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
-    dispatch(actionCreators.loginDB(body, navigate));
-  };
+    //kakao login
+    const kakaoLogin = () => {
+        window.location.href = link;
+    };
 
-  //kakao 로그인
-  const REST_API_KEY = "e1e4af98ca9f6131da2d779f616737bc";
-  const REDIRECT_URI = "http://127.0.0.1:3000/user/kakao/callback/";
-  const link = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+    //회원가입 하러 가기
+    const goSignup = () => {
+        navigate("/Signup");
+        window.location.reload();
+    };
 
-  //kakao login
-  const kakaoLogin = () => {
-    window.location.href = link;
-  };
-
-  //회원가입 하러 가기
-  const goSignup = () => {
-    navigate("/Signup");
-    window.location.reload();
-  };
-
-  return (
-    <div className="Login">
-      <h2 className="welcome">
-        <span className="modira">MODIRA</span>에 오신 것을 환영합니다!
-      </h2>
-      <form className="LoginForm" onSubmit={handleSubmit(onSubmit)}>
-        <input
-          id="email"
-          name="email"
-          type="email"
-          placeholder="E-MAIL"
-          {...register("email", {
-            required: true,
-          })}
-        />
-        <input
-          id="password"
-          name="password"
-          type="password"
-          placeholder="PASSWORD"
-          {...register("password", {
-            required: true,
-          })}
-        />
-        <button id="loginButton" type="submit">
-          LOG IN
-        </button>
-        <button id="kakaoLoginButton" type="button" onClick={kakaoLogin}>
-          <div id="kakaoLogin">
-            <img src={kakaoLogo} className="kakaoLogo" alt="kakao logo" />
-            카카오 계정으로 시작하기
-          </div>
-        </button>
-        <div className="notUser">
-          아직 회원이 아니신가요?{" "}
-          <button className="goSignup" type="button" onClick={goSignup}>
-            {" "}
-            회원가입 하러 가기
-          </button>
+    return (
+        <div className="Login">
+            <h2 className="welcome">
+                <span className="modira">MODIRA</span>에 오신 것을 환영합니다!
+            </h2>
+            <form className="LoginForm" onSubmit={handleSubmit(onSubmit)}>
+                <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    placeholder="E-MAIL"
+                    {...register("email", {
+                        required: true,
+                    })}
+                />
+                <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    placeholder="PASSWORD"
+                    {...register("password", {
+                        required: true,
+                    })}
+                />
+                <button id="loginButton" type="submit">
+                    LOG IN
+                </button>
+                <button
+                    id="kakaoLoginButton"
+                    type="button"
+                    onClick={kakaoLogin}
+                >
+                    <div id="kakaoLogin">
+                        <img
+                            src={kakaoLogo}
+                            className="kakaoLogo"
+                            alt="kakao logo"
+                        />
+                        카카오 계정으로 시작하기
+                    </div>
+                </button>
+                <div className="notUser">
+                    아직 회원이 아니신가요?{" "}
+                    <button
+                        className="goSignup"
+                        type="button"
+                        onClick={goSignup}
+                    >
+                        {" "}
+                        회원가입 하러 가기
+                    </button>
+                </div>
+            </form>
         </div>
-      </form>
-    </div>
-  );
+    );
 }
 
 export default Login;
